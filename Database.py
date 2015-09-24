f53725423364cc2f0684051e3ab980fdd14dc16a
Andreas <andreas@chaospott.de>
2015-09-24 21:09:11 +0200

    Changes to MediaDownloader

--- Database.py
+++ Database.py
@@ -1,9 +1,9 @@
 
-
 import sqlite3
 import random
 import string
 import threading
+import os
 
 def randomword(length):
     return ''.join(random.choice(string.ascii_letters) for i in range(length))
@@ -18,49 +18,56 @@ class Database:
         self.lock = threading.Lock()
 
 
+    ######################
+    # Account management #
+    ######################
     def read_accounts(self):
         with self.lock:
             c = self.database.cursor()
             c.execute("SELECT owner, number, password FROM account")
             return c.fetchall()
 
-
     def read_roster(self, owner):
         with self.lock:
             c = self.database.cursor()
             c.execute( "SELECT number FROM roster WHERE owner = ?", (owner,) )
             return c.fetchall()
-
-    def save_path(self, path, frm, msgid):
-        with self.lock:
-            rid = randomword(10)
-            c = self.database.cursor()
-            c.execute( "INSERT INTO files (path, frm, read, password) VALUES (?, ?, ?, ?)"
-                     , (path, frm, msgid, rid)
-                     )
-            return (c.lastrowid, rid)
-
-    def add_contact(self, owner, contact):
+            
+   def add_contact(self, owner, contact):
         with self.lock:
             c = self.database.cursor()
             c.execute( "INSERT INTO roster (owner, number) VALUES (?, ?)"
                      , (owner, contact)
                      )
 
-
-    def lookup_path(self, id1, id2):
+     ####################
+     # Media Downloader #
+     ####################
+    def save_path(self, path, frm, msgid):
+        with self.lock:
+            extension = os.path.splitext(path)[1]
+            # FIXME: don't do this! Might be trivial to guess these id's
+            # Should use crypto rng
+            rid = randomword(20)+"."+extension
+            c = self.database.cursor()
+            c.execute( "INSERT INTO files (path, frm, read, password) VALUES (?, ?, ?, ?)"
+                     , (path, frm, msgid, rid)
+                     )
+            return rid
+            
+     def lookup_path(self, id):
         with self.lock:
             c = self.database.cursor()
-            c.execute( "SELECT path, reciever, frm, read FROM files WHERE rowid = ? AND password = ?"
-                     , (id1, id2)
+            c.execute( "SELECT path, reciever, frm, read FROM files WHERE password = ?"
+                     , (id)
                      )
             return c.fetchone()
 
-    def set_file_read(self, id1, id2):
+    def set_file_read(self, id):
         with self.lock:
             c = self.database.cursor()
-            c.execute( "UPDATE files SET read = 0 WHERE rowid = ? AND password = ?"
-                     , (id1, id2)
+            c.execute( "UPDATE files SET read = 0 WHERE password = ?"
+                     , (id)
                      )
 
 def get_database(path):
--- MediaServer.py
+++ MediaServer.py
@@ -1,7 +1,9 @@
 
 from HttpServer import HttpRequest
 from Database import get_database
+import logging
 
+logger = logging.getLogger('watransport.mediaserver')
 
 def serve_file(accounts, config):
     return lambda sock: MediaRequest(accounts, config, sock)
@@ -20,34 +22,40 @@ class MediaRequest(HttpRequest):
         self.close()
 
     def onHeaders(self):
-        if method != "GET":
+        if self.method != "GET":
+            logger.debug("Invalid method "+self.method)
             self.error(405, "Method not allowed")
             return
-
+        
         ids = self.path.split("/")
+        #NOTE(apv): the first element of ids is always "" because the string starts with /"
         if len(ids) != 2:
+            logger.debug("Invalid path "+path+" (got ids="+str(ids)+")")
             self.error(404, "Not found")
             return
+       id = ids[1]
 
-        res = self.database.lookup_path(ids[0], ids[1])
+        res = self.database.lookup_path(id)
         if res:
+            logger.debug("Id "+str(id)+" not found in db")
             self.error(404, "Not found")
             return
 
         fpath, to, frm, read = res
-        self.serve_file(ids, fpath, read, to, frm)
+        self.serve_file(id, fpath, read, to, frm)
 
-    def serve_file(ids, fpath, read, to, frm):
+    def serve_file(id, fpath, read, to, frm):
         with open(self.path, "rb") as f:
             while True:
                 chunk = f.read(4096)
                 if not chunk:
                     break
                 self.push(chunk)
+        
         if read:
-            self.database.set_file_read(ids[0], ids[1])
+            self.database.set_file_read(ids[0])
             with self.accounts[to].xmpp.lock:
-                self.accounts[to].markWAMessageAsRead(to = frm, id = id)
+                self.accounts[to].markWAMessageAsRead(to = frm, id = read)
 
 
     def onData(self, buff):
